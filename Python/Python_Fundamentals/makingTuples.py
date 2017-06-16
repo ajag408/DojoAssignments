b22efe22d4ee54e82e9338b60d15c56c25142a5d
Akash Jag <akashjagannathan408@gmail.com>
2017-06-16 15:30:48 -0700

    making tuples

---
+++ Python/Python_Fundamentals/makingTuples.py
@@ -0,0 +1,12 @@
+def dictToTuple(dict):
+    return dict.items()
+
+
+# my_dict = {
+#   "Speros": "(555) 555-5555",
+#   "Michael": "(999) 999-9999",
+#   "Jay": "(777) 777-7777"
+# }
+#
+# answer = dictToTuple(my_dict)
+# print answer
