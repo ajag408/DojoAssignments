a80eee76a2db23587617544d2a2dbce4b9bef5c8
Akash Jag <akashjagannathan408@gmail.com>
2017-06-18 23:51:30 -0700

    finished store

--- Python/Python_OOP/store.py
+++ Python/Python_OOP/store.py
@@ -1,5 +1,5 @@
 class Store(object):
-    def __init__(self, products, location):
+    def __init__(self, products, location, owner):
         self.products = products
         self.location = location
         self.owner = owner
@@ -8,12 +8,22 @@ class Store(object):
         return self
     def remove_product(self, product_name):
         for product in self.products:
-            if product.name == product_name:
-                idx = self.products.index
+            if product['name'] == product_name:
+                idx = self.products.index(product)
                 self.products.pop(idx)
                 break
         return self
     def inventory(self):
+        print "******* Product List *********"
         for product in self.products:
             print product
         return self
+
+safeway = Store([{'name': 'milk', 'exp': '6-17'}, {'name': 'cheese', 'exp': '6-19'}], 'Sunnyvale', 'MoolahBaby')
+safeway.inventory()
+
+safeway.add_product({'name': 'toe-fungus', 'exp': '6-30'})
+safeway.inventory()
+
+safeway.remove_product('milk')
+safeway.inventory()
