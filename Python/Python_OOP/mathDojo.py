8426590240bdede36c31794c95821c3a706b0b9e
Akash Jag <akashjagannathan408@gmail.com>
2017-06-20 16:17:41 -0700

    math dojo part 2

--- Python/Python_OOP/mathDojo.py
+++ Python/Python_OOP/mathDojo.py
@@ -1,20 +1,29 @@
 class MathDojo(object):
     def __init__(self):
         self.total = 0
-    def add(self, num, *more):
-        self.total += num
-        for number in more:
-            self.total += number
+    def add(self, *args):
+        for arg in args:
+            if type(arg) is list:
+                for num in arg:
+                    self.total += num
+            else:
+                self.total += arg
         return self
-    def subtract(self, num, *more):
-        sub_total = num
-        for number in more:
-            sub_total += number
-        self.total -= sub_total
+    def subtract(self, *args):
+        for arg in args:
+            if type(arg) is list:
+                sub_total = 0
+                for num in arg:
+                    sub_total += num
+                self.total -= sub_total
+            else:
+                self.total -= arg
         return self
     def result(self):
         return self.total
 
 
-md = MathDojo().add(2).add(2,5).subtract(3,2).result()
+
+
+md = MathDojo().add([1],3,4).add([3,5,7,8],[2,4.3,1.25]).subtract(2, [2,3], [1.1, 2.3]).result()
 print md
