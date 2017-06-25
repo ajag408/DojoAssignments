3f7459c03a34b58993c4f008cd5034bea83e4293
Akash Jag <akashjagannathan408@gmail.com>
2017-06-24 17:06:21 -0700

    ajax ninjas

--- Python/Flask_Fundamentals/AJAXNinjas/server.py
+++ Python/Flask_Fundamentals/AJAXNinjas/server.py
@@ -9,4 +9,30 @@ def load_index():
 def red():
     return jsonify(img = 'imgs/raphael.jpg', text = 'You chose Raphael!')
 
+@app.route('/blue')
+def blue():
+    return jsonify(img = 'imgs/leonardo.jpg', text = 'You chose Leonardo!')
+
+@app.route('/orange')
+def orange():
+    return jsonify(img = 'imgs/michelangelo.jpg', text = 'You chose Michelangelo!')
+
+@app.route('/purple')
+def purple():
+    return jsonify(img = 'imgs/donatello.jpg', text = 'You chose Donatello!')
+
+@app.route('/form', methods = ['POST'])
+def delineate():
+    if request.form['customColor'] == 'red' or request.form['customColor'] == 'Red':
+        return render_template('index.html', text = 'You chose Raphael!', img = '/static/imgs/raphael.jpg')
+    elif request.form['customColor'] == 'blue' or request.form['customColor'] == 'Blue':
+        return render_template('index.html', text = 'You chose Leonardo!', img = '/static/imgs/leonardo.jpg')
+    elif request.form['customColor'] == 'orange' or request.form['customColor'] == 'Orange':
+        return render_template('index.html', text = 'You chose Michelangelo!', img = '/static/imgs/michelangelo.jpg')
+    elif request.form['customColor'] == 'purple' or request.form['customColor'] == 'Purple':
+        return render_template('index.html', text = 'You chose Donatello!', img = '/static/imgs/donatello.jpg')
+    else:
+        return render_template('index.html', text = "There's no ninja in that color", img = '/static/imgs/tmnt.png' )
+
+
 app.run(debug=True)
--- Python/Flask_Fundamentals/AJAXNinjas/static/js/ninjas.js
+++ Python/Flask_Fundamentals/AJAXNinjas/static/js/ninjas.js
@@ -1,16 +1,25 @@
 $(document).ready(function(){
-  $('#red').click(function(){
-    $.get('http://localhost:5000/red', function(res){
-      console.log(res.img);
-    }, 'json')
-  });
-  $('#blue').click(function(){
-
-  });
-  $('#orange').click(function(){
-
-  });
-  $('#purple').click(function(){
-
+  $('button').click(function(){
+    if($(this).text() == 'red'){
+      $.get('http://localhost:5000/red', function(res){
+        $('#picture').html('<h1>' + res.text + '</h1>')
+        $('#picture').append('<img src="/static/'+res.img+'">')
+      }, 'json')
+    } else if($(this).text() == 'blue'){
+      $.get('http://localhost:5000/blue', function(res){
+        $('#picture').html('<h1>' + res.text + '</h1>')
+        $('#picture').append('<img src="/static/'+res.img+'">')
+      }, 'json')
+    } else if($(this).text() == 'orange'){
+      $.get('http://localhost:5000/orange', function(res){
+        $('#picture').html('<h1>' + res.text + '</h1>')
+        $('#picture').append('<img src="/static/'+res.img+'">')
+      }, 'json')
+    } else if($(this).text() == 'purple'){
+      $.get('http://localhost:5000/purple', function(res){
+        $('#picture').html('<h1>' + res.text + '</h1>')
+        $('#picture').append('<img src="/static/'+res.img+'">')
+      }, 'json')
+    }
   });
 });
