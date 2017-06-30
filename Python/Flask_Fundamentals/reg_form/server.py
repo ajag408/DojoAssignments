d5eb47d2c4d86c083cacb48c4eae2caa3c43ae45
Akash Jag <akashjagannathan408@gmail.com>
2017-06-30 12:37:51 -0700

    reg form hacker

--- Python/Flask_Fundamentals/reg_form/server.py
+++ Python/Flask_Fundamentals/reg_form/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, flash
 import re
+import time
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 app = Flask(__name__)
 app.secret_key = 'KeepItSecretKeepItSafe'
@@ -31,54 +32,64 @@ def index():
   return render_template("index.html")
 @app.route('/process', methods=['POST'])
 def process():
-      if len(request.form['email']) < 1:
-          Off = True
+    if len(request.form['email']) < 1:
+          Off1 = True
           flash("email cannot be empty!", 'email')
-      elif not EMAIL_REGEX.match(request.form['email']):
-          Off = True
+    elif not EMAIL_REGEX.match(request.form['email']):
+          Off1 = True
           flash('email must be valid email', 'email')
-      else:
-          Off = False
+    else:
+          Off1 = False
 
-      if len(request.form['first_name']) < 1:
-          Off = True
+    if len(request.form['first_name']) < 1:
+          Off2 = True
           flash("name cannot be empty!", 'fName')
-      elif (request.form['first_name']).isalpha() is False:
-          Off = True
+    elif (request.form['first_name']).isalpha() is False:
+          Off2 = True
           flash("only letters please", 'fName')
-      else:
-          Off = False
+    else:
+          Off2 = False
 
-      if len(request.form['last_name']) < 1:
-          Off = True
+    if len(request.form['last_name']) < 1:
+          Off3 = True
           flash("name cannot be empty!", 'lName')
-      elif (request.form['last_name']).isalpha() is False:
-          Off = True
+    elif (request.form['last_name']).isalpha() is False:
+          Off3 = True
           flash("only letters please", 'lName')
-      else:
-          Off = False
+    else:
+          Off3 = False
 
-      if len(request.form['password']) <= 8:
-          Off = True
+    if len(request.form['password']) <= 8:
+          Off4 = True
           flash("password must be 8 characters", 'pword')
-      elif request.form['password'].isalpha() is True or request.form['password'].islower() is True:
-          Off = True
+    elif request.form['password'].isalpha() is True or request.form['password'].islower() is True:
+          Off4 = True
           flash('password must have at least one uppercase letter and at least one digit', 'pword')
-      else:
+    else:
           if len(request.form['confirm_password']) < 1:
-              Off = True
+              Off4 = True
               flash("confirm password", 'cword')
           elif request.form['password'] != request.form['confirm_password']:
-             Off = True
+             Off4 = True
              flash('password must match confirmation', 'cword')
           else:
-              Off = False
+              Off4 = False
 
+    if len(request.form['birthdate']) <= 1:
+          Off5 = True
+          flash('birthdate must be valid entry', 'bday')
+    else:
+        birthdate = time.mktime(time.strptime(request.form['birthdate'], '%Y-%m-%d'))
+        current_time = time.time()
+        if birthdate>current_time:
+            Off5 = True
+            flash('birthdate must be in past', 'bday')
+        else:
+            Off5 = False
 
-
-      if Off is False:
+    if Off1 is False and Off2 is False and Off3 is False and Off4 is False and Off5 is False:
           flash('Thank you for submitting your information', 'success')
 
-      return redirect('/')
+    return redirect('/')
 
 app.run(debug=True)
