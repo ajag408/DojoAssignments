334563cb6f40b01569c5045dc63da0595d3c7c2b
Akash Jag <akashjagannathan408@gmail.com>
2017-07-01 14:28:43 -0700

    strugs with bokeh

--- Python/Flask_Fundamentals/flaskBokeh/server.py
+++ Python/Flask_Fundamentals/flaskBokeh/server.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import file_html
+
 app = Flask(__name__)
 app.secret_key = 'KeepItSecretKeepItSafe'
 # our index route will handle rendering our form
@@ -6,5 +10,15 @@ app.secret_key = 'KeepItSecretKeepItSafe'
 @app.route('/')
 def index():
  return render_template('index.html')
+
+@app.route('/success', methods=['POST'])
+def plot():
+    plot = figure()
+    plot.circle([1,2], [3,4])
+
+    html = file_html(plot, CDN, 'my plot')
+
+    return render_template('success.html', html=html)
+
 
 app.run(debug=True)
