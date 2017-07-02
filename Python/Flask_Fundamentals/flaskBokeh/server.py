8c6a8cc327c4aa62163645882371ecbbf14e34a0
Akash Jag <akashjagannathan408@gmail.com>
2017-07-01 18:38:46 -0700

    algo for bokeh

--- Python/Flask_Fundamentals/flaskBokeh/server.py
+++ Python/Flask_Fundamentals/flaskBokeh/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
-from bokeh.plotting import figure
+from bokeh.plotting import figure, output_file, show
 from bokeh.resources import CDN
-from bokeh.embed import file_html
+
 
 app = Flask(__name__)
 app.secret_key = 'KeepItSecretKeepItSafe'
@@ -13,12 +13,41 @@ def index():
 
 @app.route('/success', methods=['POST'])
 def plot():
-    plot = figure()
-    plot.circle([1,2], [3,4])
+    x = []
+    xVals = request.form['xVals']
+    idxChk = None
+    for char in xVals:
+        print '***char****'
+        print char
+        if idxChk and xVals.index(char) < idxChk:
+            print 'continued'
+            continue
+        else:
+            if char.isdigit():
+                doubleDigit = char
+                # if xVals.index(char) < len(xVals) - 1:
+                runner = xVals.index(char) + 1
+                while runner < len(xVals):
+                    if xVals[runner].isdigit():
+                        doubleDigit = doubleDigit + xVals[runner]
+                        runner = runner + 1
+                        idxChk = runner
+                    else:
+                        break
 
-    html = file_html(plot, CDN, 'my plot')
+                print doubleDigit
+        # elif char == ',':
+        #     print char
+    # x = [1, 2, 3, 4, 5]
+    # y = [6, 7, 2, 4, 5]
+    # output_file("success.html")
+    # p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+    #
+    # p.line(x, y, line_width=2)
+    #
+    # show(p)
 
-    return render_template('success.html', html=html)
+    return redirect('/')
 
 
 app.run(debug=True)
