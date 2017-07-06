a3cd5ceabbc6172c17694fda51291c1d3f48bcef
Akash Jag <akashjagannathan408@gmail.com>
2017-07-05 17:19:54 -0700

    flask bokeh geo mapping

--- Python/Flask_Fundamentals/flaskBokeh/server.py
+++ Python/Flask_Fundamentals/flaskBokeh/server.py
@@ -1,39 +1,65 @@
 from flask import Flask, render_template, request, redirect
 from bokeh.plotting import figure, output_file, show
 from bokeh.resources import CDN
-
+from bokeh.models import (
+  GMapPlot, GMapOptions, ColumnDataSource, Circle, DataRange1d, PanTool, WheelZoomTool, BoxSelectTool
+)
 
 app = Flask(__name__)
 app.secret_key = 'KeepItSecretKeepItSafe'
-# our index route will handle rendering our form
-# ".+@.+"
+
+
+
 @app.route('/')
 def index():
  return render_template('index.html')
 
 @app.route('/success', methods=['POST'])
 def plot():
-    x = []
-    xVals = request.form['xVals']
-    for char in xVals:
-        if char.isdigit():
-            x.append(int(char))
+    # x = []
+    # xVals = request.form['xVals']
+    # for char in xVals:
+    #     if char.isdigit():
+    #         x.append(int(char))
+    #
+    # y = []
+    # yVals = request.form['yVals']
+    # for char in yVals:
+    #     if char.isdigit():
+    #         y.append(int(char))
+    #     # elif char == ',':
+    #     #     print char
+    # # x = [1, 2, 3, 4, 5]
+    # # y = [6, 7, 2, 4, 5]
+    # output_file("success.html")
+    # p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+    #
+    # p.line(x, y, line_width=2)
+    #
+    # show(p)
 
-    y = []
-    yVals = request.form['yVals']
-    for char in yVals:
-        if char.isdigit():
-            y.append(int(char))
-        # elif char == ',':
-        #     print char
-    # x = [1, 2, 3, 4, 5]
-    # y = [6, 7, 2, 4, 5]
-    output_file("success.html")
-    p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+    map_options = GMapOptions(lat=int(request.form['lat']), lng=int(request.form['lon']), map_type="roadmap", zoom=11)
 
-    p.line(x, y, line_width=2)
-    
-    show(p)
+    plot = GMapPlot(
+        x_range=DataRange1d(), y_range=DataRange1d(), map_options=map_options
+    )
+    plot.title.text = "Place"
+
+    plot.api_key = "AIzaSyB3cdvDq6ndnv1OiZwYDJBWe270FR-Wg7c"
+
+    # source = ColumnDataSource(
+    # data=dict(
+    #     lat=[30.29, 30.20, 30.29],
+    #     lon=[-97.70, -97.74, -97.78],
+    #     )
+    # )
+    #
+    # circle = Circle(x="lon", y="lat", size=15, fill_color="blue", fill_alpha=0.8, line_color=None)
+    # plot.add_glyph(source, circle)
+
+    plot.add_tools(PanTool(), WheelZoomTool(), BoxSelectTool())
+    output_file("gmap_plot.html")
+    show(plot)
 
     return redirect('/')
 
