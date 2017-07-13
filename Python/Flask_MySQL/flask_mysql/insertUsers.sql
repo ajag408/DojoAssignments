a4d607ccdef28ccacdc88ca251d6904015a18a86
Akash Jag <akashjagannathan408@gmail.com>
2017-07-13 16:15:26 -0700

    starting mysql with flask assignments

---
+++ Python/Flask_MySQL/flask_mysql/insertUsers.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO users (name)
+-- VALUES('Akash')
+
+-- INSERT INTO users (name)
+-- VALUES('Anisha')
+
+INSERT INTO users (name)
+VALUES('Jagan')
--- Python/Flask_MySQL/flask_mysql/server.py
+++ Python/Flask_MySQL/flask_mysql/server.py
@@ -3,7 +3,7 @@ from flask import Flask
 from mysqlconnection import MySQLConnector
 app = Flask(__name__)
 # connect and store the connection in "mysql" note that you pass the database name to the function
-mysql = MySQLConnector(app, 'twitter')
+mysql = MySQLConnector(app, 'mydb')
 # an example of running a query
 print mysql.query_db("SELECT * FROM users")
 app.run(debug=True)
