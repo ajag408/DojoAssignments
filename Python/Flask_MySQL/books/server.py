ca4c34449ffef569f6a9d21d3e0deb6c940840af
Akash Jag <akashjagannathan408@gmail.com>
2017-07-15 12:32:59 -0700

    finished normal version of books

--- Python/Flask_MySQL/books/server.py
+++ Python/Flask_MySQL/books/server.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request, redirect
 from mysqlconnection import MySQLConnector
+import datetime
 
 app = Flask(__name__)
 mysql = MySQLConnector(app,'books')
 
 @app.route('/')
 def index():
-  return render_template("index.html")
+  query = "SELECT * FROM books"
+  books = mysql.query_db(query)
+  for book in books:
+      book['created_at'] = book['created_at'].strftime('%b %dth %Y')
+  return render_template("index.html", books = books)
 
 @app.route('/render_add')
 def render_add():
@@ -14,6 +19,31 @@ def render_add():
 
 @app.route('/add', methods=['POST'])
 def add():
+    query = "INSERT INTO books (title, author, created_at, updated_at)\
+             VALUES (:title, :author, NOW(), NOW())"
 
+    data = {
+            'title': request.form['title'],
+            'author': request.form['author'] }
+
+    mysql.query_db(query, data)
+
+    return redirect('/')
+
+@app.route('/render_delete/<id>')
+def render_delete(id):
+    query = "SELECT * FROM books WHERE id = :id"
+    data = {
+            'id': id }
+    book = mysql.query_db(query,data)
+    print book
+    return render_template('delete.html', book = book)
+
+@app.route('/delete/<id>')
+def delete(id):
+    query = "DELETE FROM books WHERE id = :id"
+    data = {'id': id}
+    mysql.query_db(query, data)
+    return redirect('/')
 
 app.run(debug=True)
