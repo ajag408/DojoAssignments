454baa48673408c4c08d7bd681937edf9fd64a24
Akash Jag <akashjagannathan408@gmail.com>
2017-07-18 17:29:14 -0700

    finished books_and_quotes

--- Python/Flask_MySQL/books_and_quotes/server.py
+++ Python/Flask_MySQL/books_and_quotes/server.py
@@ -41,9 +41,60 @@ def render_delete(id):
 
 @app.route('/delete/<id>')
 def delete(id):
-    query = "DELETE FROM books WHERE id = :id"
-    data = {'id': id}
-    mysql.query_db(query, data)
+    query2 = "DELETE FROM quotes WHERE book_id = :book_id"
+    data2 = {'book_id': id}
+    mysql.query_db(query2, data2)
+
+    query1 = "DELETE FROM books WHERE id = :id"
+    data1 = {'id': id}
+    mysql.query_db(query1, data1)
+
     return redirect('/')
 
- app.run(debug=True)
+@app.route('/add_quote/<id>')
+def render_add_quote(id):
+    query = "SELECT * FROM books WHERE id = :id"
+
+    data = {
+            'id': id
+    }
+
+    book = mysql.query_db(query, data)
+    book = book[0]
+
+    return render_template('add_quote.html', book = book)
+
+@app.route('/add_quote/<id>', methods = ['POST'])
+def add_quote(id):
+    query = "INSERT INTO quotes (book_id, quote, created_at, updated_at)\
+             VALUES (:book_id, :quote, NOW(), NOW())"
+
+    data = {
+            'book_id': id,
+            'quote': request.form['quote']
+    }
+
+    mysql.query_db(query,data)
+
+    return redirect('/')
+
+@app.route('/quotes/<id>')
+def view_quotes(id):
+    query1 = "SELECT title FROM books WHERE id = :id"
+    data1 = {
+            'id': id
+    }
+    book = mysql.query_db(query1,data1)
+    book = book[0]
+
+    query2 = "SELECT quote FROM quotes WHERE book_id = :book_id"
+
+    data2 = {
+            'book_id': id
+    }
+
+    quotes = mysql.query_db(query2, data2)
+
+    return render_template('quotes.html', book = book, quotes = quotes)
+
+app.run(debug=True)
