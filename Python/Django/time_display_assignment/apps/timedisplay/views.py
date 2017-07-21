0d839ef982510885c429d9228a3a3971fff9a99c
Akash Jag <akashjagannathan408@gmail.com>
2017-07-20 21:49:37 -0700

    fixed time display to show pst

--- Python/Django/time_display_assignment/apps/timedisplay/views.py
+++ Python/Django/time_display_assignment/apps/timedisplay/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, HttpResponse
-import datetime
+from datetime import datetime
+from pytz import timezone
+import pytz
 
 # Create your views here.
 def timedisplay(request):
+    date_format="%Y-%m-%d %H:%M %p"
+    date = datetime.now(tz=pytz.utc)
+    date = date.astimezone(timezone('US/Pacific'))
+    current_time = date.strftime(date_format)
+
     context = {
-        'current_time': datetime.datetime.now()
+        'current_time': current_time
     }
     return render(request, 'timedisplay/index.html', context)
