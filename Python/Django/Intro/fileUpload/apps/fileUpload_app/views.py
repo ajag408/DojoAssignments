dcbca8996567309ba6085e3cdf81cb67da602f23
Akash Jag <akashjagannathan408@gmail.com>
2017-07-24 00:12:15 -0700

    finished file upload

--- Python/Django/Intro/fileUpload/apps/fileUpload_app/views.py
+++ Python/Django/Intro/fileUpload/apps/fileUpload_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django import forms
+
 
 # Create your views here.
+def handle_uploaded_file(f):
+    with open('./apps/fileUpload_app/upload', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+def index(request):
+    return render(request, 'fileUpload_app/index.html')
+
+def upload(request):
+    if 'file' in request.FILES:
+        handle_uploaded_file(request.FILES['file'])
+
+    return redirect('/')
