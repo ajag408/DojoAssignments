f58223fbab301ee7d55a6760489447459b64f632
Akash Jag <akashjagannathan408@gmail.com>
2017-07-25 15:08:44 -0700

    working on users

--- Python/Django/users/apps/users_app/urls.py
+++ Python/Django/users/apps/users_app/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
-    url(r'^$', views.index)     # This line has changed!
+    url(r'^$', views.index),
+    url(r'^users$', views.users_home),
+    url(r'^users/new$', views.render_add_user),
+    url(r'^users/create$', views.add_user)
 ]
--- Python/Django/users/apps/users_app/views.py
+++ Python/Django/users/apps/users_app/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from models import *
 
 # Create your views here.
 def index(request):
-    return
+    return redirect('/users')
+
+def users_home(request):
+    context = {
+        'users': User.objects.all()
+    }
+    return render(request, 'users_app/index.html', context)
+
+def render_add_user(request):
+    return render(request, 'users_app/new.html')
