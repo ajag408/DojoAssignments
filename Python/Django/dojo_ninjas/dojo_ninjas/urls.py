6e08f83c5d84ffcad019524842b23ffc087af2a0
Akash Jag <akashjagannathan408@gmail.com>
2017-07-24 23:00:11 -0700

    books and authors orm

--- Python/Django/dojo_ninjas/dojo_ninjas/urls.py
+++ Python/Django/dojo_ninjas/dojo_ninjas/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('apps.orm_app.urls'))
+    url(r'^', include('apps.orm_app.urls')),
+    url(r'^', include('apps.books_authors.urls'))
 ]
