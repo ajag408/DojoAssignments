934294d5a1153a95e114948932cf85918b918b6b
Akash Jag <akashjagannathan408@gmail.com>
2017-07-25 22:00:26 -0700

    users assignment

--- Python/Django/users/apps/users_app/urls.py
+++ Python/Django/users/apps/users_app/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^users$', views.users_home),
-    url(r'^users/new$', views.render_add_user),
-    url(r'^users/create$', views.add_user)
+    url(r'^$', views.render_users),
+    url(r'^users$', views.index),
+    url(r'^users/new$', views.new),
+    url(r'^users/(?P<user_id>\d+)/edit$', views.edit),
+    url(r'^users/(?P<user_id>\d+)$', views.show),
+    url(r'^users/create$', views.create),
+    url(r'^users/(?P<user_id>\d+)/destroy$', views.destroy)
 ]
--- Python/Django/users/apps/users_app/views.py
+++ Python/Django/users/apps/users_app/views.py
@@ -2,14 +2,46 @@ from django.shortcuts import render, redirect
 from models import *
 
 # Create your views here.
-def index(request):
+def render_users(request):
     return redirect('/users')
 
-def users_home(request):
+def index(request):
     context = {
         'users': User.objects.all()
     }
     return render(request, 'users_app/index.html', context)
 
-def render_add_user(request):
+def new(request):
     return render(request, 'users_app/new.html')
+
+def edit(request, user_id):
+    context = {
+        'user': User.objects.get(id = user_id)
+    }
+    return render(request, 'users_app/edit.html', context)
+
+def show(request, user_id):
+    user = User.objects.get(id = user_id)
+    if request.method == 'POST':
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+    context = {
+        'user': user
+    }
+    return render(request, 'users_app/show.html', context)
+
+
+def create(request):
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    email = request.POST['email']
+    User.objects.create(first_name = first_name, last_name = last_name, email = email)
+    return redirect('/users')
+
+def destroy(request, user_id):
+    done_for = User.objects.get(id = user_id)
+    done_for.delete()
+    return redirect('/users')
