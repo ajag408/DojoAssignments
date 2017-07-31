65f6285b51ce1338faef7949f89064aa5f595147
Akash Jag <akashjagannathan408@gmail.com>
2017-07-31 15:34:29 -0700

    django ajax demo 1

--- Python/Django/AJAX/user_login/apps/orm_app/urls.py
+++ Python/Django/AJAX/user_login/apps/orm_app/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
-    url(r'^$', views.index)     # This line has changed!
+    url(r'^$', views.index),
+    url(r'^all.json$', views.all_json),
+    url(r'^all.html$', views.all_html)
 ]
--- Python/Django/AJAX/user_login/apps/orm_app/views.py
+++ Python/Django/AJAX/user_login/apps/orm_app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render, HttpResponse, redirect
 from models import *
+from django.core import serializers
 # the index function is called when root is visited
 def index(request):
-    response = "Hello, I am your first request!"
-    return HttpResponse(response)
+    return render(request, 'orm_app/index.html')
+
+def all_json(request):
+    users = User.objects.all()
+    users_json = serializers.serialize('json', users)
+    return HttpResponse(users_json, content_type = 'application/json')
+
+def all_html(request):
+    users = User.objects.all()
+    return render(request, 'orm_app/all.html', {'users': users})
