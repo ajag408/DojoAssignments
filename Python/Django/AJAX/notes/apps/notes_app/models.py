b65ac243cdc33b1030213ed172c0cdfbb7340e05
Akash Jag <akashjagannathan408@gmail.com>
2017-08-01 00:04:18 -0700

    notes

--- Python/Django/AJAX/notes/apps/notes_app/models.py
+++ Python/Django/AJAX/notes/apps/notes_app/models.py
@@ -3,6 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class NoteManager(models.Manager):
+    def isValid(self,postData):
+        if len(postData['title']) == 0:
+            return False
+        else:
+            return True
 class Note(models.Model):
     title = models.CharField(max_length = 255)
     description = models.TextField()
+    objects = NoteManager()
--- Python/Django/AJAX/notes/apps/notes_app/views.py
+++ Python/Django/AJAX/notes/apps/notes_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib import messages
 from models import *
 # Create your views here.
 def index(request):
@@ -6,7 +7,12 @@ def index(request):
     return render(request, 'notes_app/index.html', {'notes': notes})
 
 def add_note(request):
-    Note.objects.create(title = request.POST['title'], description = '')
+    isValid = Note.objects.isValid(request.POST)
+    if isValid is False:
+        messages.error(request, "Please enter something")
+    else:
+        Note.objects.create(title = request.POST['title'], description = '')
+        messages.success(request, 'Note added')
     notes = Note.objects.all()
     return render(request, 'notes_app/fresh_note.html', {'notes': notes})
 
