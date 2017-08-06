6e51a39fe2902250d7fe5515ec7156cb952e9905
Akash Jag <akashjagannathan408@gmail.com>
2017-08-05 17:48:28 -0700

    iTunes

--- Python/Django/AJAX/iTunes/apps/searchApp/views.py
+++ Python/Django/AJAX/iTunes/apps/searchApp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse, redirect
+import requests
 # Create your views here.
+def index(request):
+    return render(request, 'searchApp/index.html')
+
+def get_movie(request):
+    artist = request.POST['user_input'].replace(' ', '')
+    url = "https://itunes.apple.com/search?term=" + artist + "&entity=musicVideo"
+    # notice this is 'requests' not 'request'
+    # we are using the requests module 'get' function to send a request from our server
+    # then we use ".content" to get the content we are looking for
+    response = requests.get(url).content
+    # we then send the response back to our template which sent the initial post request
+    # we don't jsonify it as it is already in JSON format!
+    return HttpResponse(response, content_type='application/json')
