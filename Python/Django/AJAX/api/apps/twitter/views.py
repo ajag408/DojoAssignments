b2451d3b183d03f42e4ed71e7c62b007cde26176
Akash Jag <akashjagannathan408@gmail.com>
2017-08-21 11:12:58 -0700

    working on api

--- Python/Django/AJAX/api/apps/twitter/views.py
+++ Python/Django/AJAX/api/apps/twitter/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'twitter/home.html')
+
+def tweets(request):
+    print request.POST['name']
