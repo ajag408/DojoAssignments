2d83a337d14d41d5ed1a9d8b824d3144f3d88ffa
Akash Jag <akashjagannathan408@gmail.com>
2017-08-22 15:21:45 -0700

    working on slist reverse

--- Python/Python_Extras/Algos/slists.py
+++ Python/Python_Extras/Algos/slists.py
@@ -28,15 +28,56 @@ class SList(object):
         newHead.next = self.head
         self.head = newHead
     def InsertBefore(self, nextVal, val):
+        if self.head.value == nextVal:
+            newNode = SLNode(val)
+            newNode.next = self.head
+            self.head = newNode
+        else:
+            runner = self.head
+            while runner.next != None and runner.next.value != nextVal:
+                runner = runner.next
+            if runner.next == None:
+                print 'no node of the value nextVal exists in this list!'
+            else:
+                newNode = SLNode(val)
+                newNode.next = runner.next
+                runner.next = newNode
+    def InsertAfter(self, preval, val):
         runner = self.head
-        while runner.next != None and runner.next.value != nextVal:
+        while runner.next != None and runner.next.value != preval:
             runner = runner.next
         if runner.next == None:
-            print 'no node of the value nextVal exists in this list!'
+            print 'no node of the value preval exists in this list!'
         else:
             newNode = SLNode(val)
-            newNode.next = runner.next
-            runner.next = newNode
+            newNode.next = runner.next.next
+            runner.next.next = newNode
+    def RemoveNode(self, val):
+        if self.head.value == val:
+            if self.head.next == None:
+                self.head = None
+            else:
+                self.head = self.head.next
+        else:
+            runner = self.head
+            while runner.next != None and runner.next.value != val:
+                runner = runner.next
+            if runner.next == None:
+                print 'no such node to remove'
+            else:
+                fucked = runner.next
+                runner.next = fucked.next
+    def ReverseList(self):
+        storeArr = [self.head]
+        runner = self.head
+        while runner.next != None:
+            storeArr.append(runner.next)
+            runner = runner.next
+        self.head = storeArr[len(storeArr)-1]
+        self.head.next = storeArr[len(storeArr)-2]
+        runner = self.head
+        for idxInc in range(2, len(storeArr)+1):
+
 
 list = SList()
 list.head = SLNode('Jameson')
@@ -44,5 +85,5 @@ list.head.next = SLNode('Jon')
 list.head.next.next = SLNode('Steve')
 
 
-list.InsertBefore('Jon', 'Tyrese')
+list.RemoveNode('Steve')
 list.PrintAllVals()
