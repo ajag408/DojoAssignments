cfb032c9db4426d7af77ad52a8e18dfc5946c7ff
Akash Jag <akashjagannathan408@gmail.com>
2017-08-28 21:58:10 -0700

    starting advanced version

--- Python/Django/Django_extras/AJAX/pokemon/apps/pokeApp/views.py
+++ Python/Django/Django_extras/AJAX/pokemon/apps/pokeApp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'pokeApp/index.html')
+    return redirect('/login_reg/')
