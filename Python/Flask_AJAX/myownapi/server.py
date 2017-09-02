fe63dd4e6208bebf21527e8eb23610ff8740aef2
Akash Jag <akashjagannathan408@gmail.com>
2017-09-01 21:03:22 -0700

    my own api create

--- Python/Flask_AJAX/myownapi/server.py
+++ Python/Flask_AJAX/myownapi/server.py
@@ -12,17 +12,25 @@ mysql = MySQLConnector(app, 'myownapi')
 
 @app.route('/quotes')
 def index():
-    return render_template('index.html')
-#  JSON-oriented index method
+    query = "SELECT * FROM quotes"
+    quotes = mysql.query_db(query)
+    return render_template('index.html', quotes=quotes)
 @app.route('/quotes/index_json')
 def index_json():
     query = "SELECT * FROM quotes"
     quotes = mysql.query_db(query)
     return jsonify(quotes=quotes)
-    
+
 @app.route('/quotes/index_html')
 def index_partial():
     query = "SELECT * FROM quotes"
     quotes = mysql.query_db(query)
     return render_template('partials/quotes.html', quotes=quotes)
+
+@app.route('/quotes/create', methods=['POST'])
+def create():
+    quote = request.form
+    query = "INSERT INTO quotes(author, quote) VALUES('{}','{}')".format(quote['author'], quote['quote'])
+    mysql.query_db(query)
+    return redirect('/quotes')
 app.run(debug=True)
