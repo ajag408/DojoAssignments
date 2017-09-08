7265df61ef2121bb0f20847dd401c8f8b3005173
Akash Jag <akashjagannathan408@gmail.com>
2017-09-07 23:52:02 -0700

    working on getting pic to show

--- Python/Django/Intro/face_detection/apps/face_detection_app/views.py
+++ Python/Django/Intro/face_detection/apps/face_detection_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django import forms
+from django.contrib import messages
 import os
 import cv2
 import sys
@@ -7,7 +8,7 @@ import sys
 
 # Create your views here.
 def handle_uploaded_file(f):
-    with open('./apps/face_detection_app/upload.png', 'wb+') as destination:
+    with open('./apps/face_detection_app/static/face_detection_app/img/upload.png', 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
 
@@ -19,7 +20,7 @@ def upload(request):
         handle_uploaded_file(request.FILES['file'])
         # os.system('python ./apps/face_detection_app/face_detect_cv3.py ./apps/face_detection_app/upload.png ./apps/face_detection_app/haarcascade_frontalface_default.xml
         # Get user supplied values
-        imagePath = "./apps/face_detection_app/upload.png"
+        imagePath = './apps/face_detection_app/static/face_detection_app/img/upload.png'
         cascPath = "./apps/face_detection_app/haarcascade_frontalface_default.xml"
 
         # Create the haar cascade
@@ -39,5 +40,6 @@ def upload(request):
         )
 
         print("Found {0} faces!".format(len(faces)))
-        request.session['faces'] = len(faces)
+        messages.info(request, imagePath, extra_tags='photo')
+        messages.info(request, 'Found ' + str(len(faces)) + ' faces!')
     return redirect('/')
