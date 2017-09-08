50ff4825ec1256e71aabd1739559aefada020ea3
Akash Jag <akashjagannathan408@gmail.com>
2017-09-07 17:21:46 -0700

    working on facedetection

--- Python/Django/Intro/face_detection/apps/face_detection_app/views.py
+++ Python/Django/Intro/face_detection/apps/face_detection_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django import forms
 import os
+import cv2
+import sys
 
 
 # Create your views here.
@@ -15,6 +17,27 @@ def index(request):
 def upload(request):
     if 'file' in request.FILES:
         handle_uploaded_file(request.FILES['file'])
-        os.system('python ./apps/face_detection_app/face_detect_cv3.py ./apps/face_detection_app/upload.png ./apps/face_detection_app/haarcascade_frontalface_default.xml')
+        # os.system('python ./apps/face_detection_app/face_detect_cv3.py ./apps/face_detection_app/upload.png ./apps/face_detection_app/haarcascade_frontalface_default.xml
+        # Get user supplied values
+        imagePath = "./apps/face_detection_app/upload.png"
+        cascPath = "./apps/face_detection_app/haarcascade_frontalface_default.xml"
 
+        # Create the haar cascade
+        faceCascade = cv2.CascadeClassifier(cascPath)
+
+        # Read the image
+        image = cv2.imread(imagePath)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces in the image
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+            #flags = cv2.CV_HAAR_SCALE_IMAGE
+        )
+
+        print("Found {0} faces!".format(len(faces)))
+        request.session['faces'] = len(faces)
     return redirect('/')
