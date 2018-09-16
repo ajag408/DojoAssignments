4e9df6ef288905436c4b7822d35c99c55fd5f402
Akash Jag <akashjagannathan408@gmail.com>
2018-09-16 12:48:14 -0700

    java string manipulation exercise

--- Java_Spring/Java_Fundamentals/StringManipulation/StringManipulator.java
+++ Java_Spring/Java_Fundamentals/StringManipulation/StringManipulator.java
@@ -7,6 +7,26 @@ public class StringManipulator {
     }
 
     public Integer getIndexOrNull(String word, char letter){
-      
+      int index = word.indexOf(letter);
+      if(index >= 0){
+        return index;
+      } else{
+        return null;
+      }
+    }
+
+    public Integer getIndexOrNull(String word, String substring){
+      int index = word.indexOf(substring);
+      if(index >= 0){
+        return index;
+      } else{
+        return null;
+      }
+    }
+
+    public String concatSubstring(String first, int one, int two, String second){
+      String subby = first.substring(one, two);
+      String concat = subby + second;
+      return concat;
     }
   }
