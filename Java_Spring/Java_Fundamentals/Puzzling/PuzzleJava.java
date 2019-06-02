0e51e6f0ab3c0b8ee996cf5d622811f2e5510232
Akash Jag <akashjagannathan408@gmail.com>
2019-06-02 12:59:33 -0700

    finished puzzling

--- Java_Spring/Java_Fundamentals/Puzzling/PuzzleJava.java
+++ Java_Spring/Java_Fundamentals/Puzzling/PuzzleJava.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
+import java.nio.charset.Charset;
 public class PuzzleJava{
     // public String trimAndConcat(String one, String two) {
     //   String oneTrim = one.trim();
@@ -44,5 +45,54 @@ public class PuzzleJava{
 
     }
 
+    public void puzzleThree(ArrayList<Character> arr){
+      Collections.shuffle(arr);
+      System.out.println("Last one: " + arr.get(25));
+      char first = arr.get(0);
+      System.out.println("First one: " + first);
+      if(first == 'a' || first == 'e' || first == 'i' || first == 'o' || first == 'u'){
+        System.out.println("First is a vowel");
+      }
+    }
+
+    public int[] puzzleFour(){
+      Random r = new Random();
+      int[] randArr = new int[10];
+      for(int i = 0; i<10; i++){
+        // System.out.println(r.nextInt(46));
+        randArr[i] = r.nextInt(46) + 55;
+      }
+      return randArr;
+    }
+
+    public ArrayList<Integer> puzzleFive(){
+      Random r = new Random();
+      ArrayList<Integer> randArr = new ArrayList<Integer>();
+      for(int i = 0; i<10; i++){
+        // System.out.println(r.nextInt(46));
+        randArr.add(r.nextInt(46) + 55);
+      }
+      Collections.sort(randArr);
+      return randArr;
+    }
+
+    public String puzzleSix(){
+      byte[] array = new byte[5];
+      new Random().nextBytes(array);
+      String generatedString = new String(array, Charset.forName("UTF-8"));
+
+      return generatedString;
+    }
+
+    public ArrayList<String> puzzleSeven(){
+      ArrayList<String> randArr = new ArrayList<String>();
+      for(int i = 0; i<10; i++){
+        byte[] array = new byte[5];
+        new Random().nextBytes(array);
+        String generatedString = new String(array, Charset.forName("UTF-8"));
+        randArr.add(generatedString);
+      }
+      return randArr;
+    }
 
 }
--- Java_Spring/Java_Fundamentals/Puzzling/PuzzleJavaTest.java
+++ Java_Spring/Java_Fundamentals/Puzzling/PuzzleJavaTest.java
@@ -17,6 +17,33 @@ public class PuzzleJavaTest {
       // ArrayList<String> names = puzzling.puzzleTwo(myArr);
       // System.out.println(names);
 
-      
+      // ArrayList<Character> myArr = new ArrayList<Character>();
+      // Collections.addAll(myArr, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+      // puzzling.puzzleThree(myArr);
+
+      // int[] myArr = puzzling.puzzleFour();
+      // for(int i = 0; i<10; i++){
+      //   System.out.println(myArr[i]);
+      // }
+
+      // ArrayList<Integer> myArr = puzzling.puzzleFive();
+      // System.out.println(myArr);
+      // int max = myArr.get(0);
+      // int min = myArr.get(0);
+      // for(int i = 1; i<10; i++){
+      //   if(myArr.get(i)>max){
+      //     max = myArr.get(i);
+      //   }
+      //   if(myArr.get(i)<min){
+      //     min = myArr.get(i);
+      //   }
+      // }
+      // System.out.println("Min: " + min + " max: " + max);
+
+      // String s = puzzling.puzzleSix();
+      // System.out.println(s);
+
+      ArrayList<String> myArr = puzzling.puzzleSeven();
+      System.out.println(myArr);
     }
 }
