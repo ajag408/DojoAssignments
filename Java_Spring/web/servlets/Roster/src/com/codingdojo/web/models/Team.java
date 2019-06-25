138b06b1db8c0832372f3daa5ceae46a2a3698e3
Akash Jag <akashjagannathan408@gmail.com>
2019-06-25 13:24:37 -0700

    one more

--- Java_Spring/web/servlets/Roster/src/com/codingdojo/web/models/Team.java
+++ Java_Spring/web/servlets/Roster/src/com/codingdojo/web/models/Team.java
@@ -4,4 +4,17 @@ import java.util.ArrayList;
 public class Team {
 	private String name;
 	ArrayList<Player> playerList = new ArrayList<Player>();
+	
+	public Team() {
+	}
+	
+	public Team(String name) {
+		this.name = name;
+	}
+	
+	public void addPlayer(Player player) {
+		playerList.add(player);
+	}
+	
+	
 }
