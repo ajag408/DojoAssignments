14a651426c7c35a85d70113e1c83994e5aa77636
Akash Jag <akashjagannathan408@gmail.com>
2019-07-15 16:39:00 -0700

    first couple projects

--- Java_Spring/Spring/Fun/strings/src/main/java/com/akash/strings/StringsApplication.java
+++ Java_Spring/Spring/Fun/strings/src/main/java/com/akash/strings/StringsApplication.java
@@ -1,4 +1,4 @@
-package com.akash.firstproject;
+package com.akash.strings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,15 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class FirstprojectApplication {
+public class StringsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstprojectApplication.class, args);
+		SpringApplication.run(StringsApplication.class, args);
 	}
-
+	
     @RequestMapping("/")
-    // 3. Method that maps to the request route above
-    public String hello() { // 3
-            return "Hello World!";
+    public String root() { // 3
+        return "Hello client! How are you doing?";
+    }
+    
+    @RequestMapping("/random")
+    public String shpeel() { // 3
+        return "Spring boot is great! So easy to respond with strings";
     }
 }
+
