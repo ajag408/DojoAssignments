d81da09742bab5c3b05ffb0e7ae6dd63421a5177
Akash Jag <akashjagannathan408@gmail.com>
2019-07-16 09:54:53 -0700

    fixed bug

--- Java_Spring/Spring/Fun/controllerspractice/src/main/java/com/akash/controllerspractice/DojoController.java
+++ Java_Spring/Spring/Fun/controllerspractice/src/main/java/com/akash/controllerspractice/DojoController.java
@@ -7,14 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class DojoController {
 	@RequestMapping("/{locay}")
     public String renderPage(@PathVariable("locay") String locay){
-		System.out.println(locay);
-		System.out.println(locay == "burbank-dojo");
-    	if(locay == "dojo") {
+//		System.out.println(locay);
+//		locay = (String) locay;
+//		System.out.println(locay == "burbank-dojo");
+    	if(locay.equals("dojo")) {
     		return "The dojo is awesome!";
-    	} else if (locay == "burbank-dojo") {
-    		System.out.println("this should print");
+    	} else if (locay.equals("burbank-dojo")) {
+//    		System.out.println("this should print");
     		return "Burbank Dojo is located in Southern California";
-    	} else if (locay == "san-jose") {
+    	} else if (locay.equals("san-jose")) {
     		return "SJ dojo is the headquarters";
     	}else {
     		return null;
