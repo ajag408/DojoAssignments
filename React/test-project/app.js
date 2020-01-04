795a0ede34e8b53455d2b0ab5449b0731a0e0fcf
Akash Jag <akashjagannathan408@gmail.com>
2020-01-03 17:01:28 -0800

    hello dojo

--- React/test-project/app.js
+++ React/test-project/app.js
@@ -1,16 +1,12 @@
-const myButton = React.createElement('button', null, 'Click Me');
-console.log(JSON.stringify(myButton));
-/*
-  Prints the following:
-  {
-  "type":"button",
-  "key":null,
-  "ref":null,
-  "props": {
-  "children":"Click Me!"
-  },
-  "_owner":null,
-  "_store":{}
-  }
-*/
-ReactDOM.render(myButton, document.getElementById('app'));
+
+// const header = React.createElement('h1', null, 'Hello Dojo!');
+ReactDOM.render(React.createElement("div", null, React.createElement('h1', null, 'Hello Dojo!'),
+    React.createElement('h3', null, 'Goals:'),
+    React.createElement('ul', null,
+        React.createElement('li', null, 'Be happpy'), React.createElement('li', null, 'Null'),
+      React.createElement('li', null, 'Null'), React.createElement('li', null, 'Null'))), document.getElementById('app'));
+// ReactDOM.render(React.createElement('h1', null, 'Hello Dojo!'), document.getElementById('app'));
+// ReactDOM.render(React.createElement('h3', null, 'Goals:'), document.getElementById('app'));
+// ReactDOM.render(React.createElement('ul', null,
+//     React.createElement('li', null, 'Be happpy'), React.createElement('li', null, 'Null'),
+//   React.createElement('li', null, 'Null'), React.createElement('li', null, 'Null')), document.getElementById('app'));
